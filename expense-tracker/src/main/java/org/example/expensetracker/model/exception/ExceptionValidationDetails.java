245e1fa00a521c1771c42180006d53d1ea7067c1
Shkurko Danylo <90965475+DanyloShkurko@users.noreply.github.com>
2024-11-05 22:47:21 +0200

    Added logic for checking expenses and limits. Also customized exception handling. And removed the id field from the models that are in the request folder

---
+++ expense-tracker/src/main/java/org/example/expensetracker/model/exception/ExceptionValidationDetails.java
@@ -0,0 +1,19 @@
+package org.example.expensetracker.model.exception;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ExceptionValidationDetails extends ExceptionDetails {
+    private Map<String, String> validationErrors;
+    public ExceptionValidationDetails(LocalDateTime timestamp, String message, String details, Map<String, String> validationErrors) {
+        super(timestamp, message, details);
+        this.validationErrors = validationErrors;
+    }
+}
--- expense-tracker/src/main/java/org/example/expensetracker/model/request/limit/LimitRequest.java
+++ expense-tracker/src/main/java/org/example/expensetracker/model/request/limit/LimitRequest.java
@@ -1,5 +1,6 @@
 package org.example.expensetracker.model.request.limit;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,16 +9,32 @@ import org.example.expensetracker.entity.Category;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LimitRequest {
+    @NotNull(message = "Limit amount is required.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Limit amount must be greater than zero.")
     private BigDecimal limitAmount;
+
+    @DecimalMin(value = "0.0", message = "Current spent amount cannot be negative.")
     private BigDecimal currentSpent;
+
     private boolean isExceeded;
+
+    @NotNull(message = "Category is required.")
     private Category category;
+
+    @NotNull(message = "Start date is required.")
+    @PastOrPresent(message = "Start date cannot be in the future.")
     private LocalDate startDate;
+
+    @NotNull(message = "End date is required.")
+    @FutureOrPresent(message = "End date cannot be in the past.")
     private LocalDate endDate;
+
+    @Positive(message = "User ID must be a positive value.")
     private long userId;
 }
