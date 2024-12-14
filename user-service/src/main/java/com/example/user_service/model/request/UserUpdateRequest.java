8fe6c576cef2672338beaf3fc500ce2161a59651
Shkurko Danylo <90965475+DanyloShkurko@users.noreply.github.com>
2024-12-14 18:58:01 +0200

    Add Swagger annotations to UserResponse and UserUpdateRequest
    Enhanced the `UserResponse` and `UserUpdateRequest` models with Swagger annotations to improve API documentation. These changes provide detailed descriptions, examples, and validation metadata for better clarity and usability of the API.

--- user-service/src/main/java/com/example/user_service/model/request/UserUpdateRequest.java
+++ user-service/src/main/java/com/example/user_service/model/request/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.example.user_service.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,16 +13,31 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Model representing a request to update user details")
 public class UserUpdateRequest {
+
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Schema(description = "The username of the user",
+            example = "john_doe",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            minLength = 3,
+            maxLength = 50)
     private String username;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
+    @Schema(description = "The email address of the user",
+            example = "john.doe@example.com",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            format = "email")
     private String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Schema(description = "The password for the user account. It must be at least 8 characters long.",
+            example = "P@ssw0rd!",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            minLength = 8)
     private String password;
 }
