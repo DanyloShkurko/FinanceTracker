ca31bc0fbb8d6196723b04f075e94469155a8bd1
Shkurko Danylo <90965475+DanyloShkurko@users.noreply.github.com>
2024-12-16 18:45:40 +0200

    Create Dockerfiles for multiple services and update dependencies
    Added Dockerfiles for user-service, expense-tracker, auth-service, and eureka-server and finance-tracker-web-client.

--- finance-tracker-web-client/src/App.tsx
+++ finance-tracker-web-client/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import LoginComponent from "./components/authComponents/LoginComponent.tsx";
 import AuthProvider from "./components/security/AuthProvider.tsx";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import {useAuth} from "./components/security/AuthContext.tsx";
 import {ReactNode, useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
