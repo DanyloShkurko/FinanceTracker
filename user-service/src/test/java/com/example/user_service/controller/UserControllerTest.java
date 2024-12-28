10feb2d2b6be57323c08da5aa83ce0efeff643a9
Shkurko Danylo <90965475+DanyloShkurko@users.noreply.github.com>
2024-12-28 13:57:05 +0200

    Prepared user-service for integration testing.

---
+++ user-service/src/test/java/com/example/user_service/controller/UserControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.user_service.controller;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+    @Test
+    void fetchUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+}
